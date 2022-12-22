fb3fdfa94415089bc42b1834858bff665e49be8c
dashevskydmytro@gmail.com <dashevskydmytro@gmail.com>
2022-12-22 21:39:09 +0200

    done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,42 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(SimpleLightbox);
+const container = document.querySelector('.gallery');
+const galleryMarkup = createGallery();
+container.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGallery() {
+  return galleryItems
+    .map(({ original, preview, description }) => {
+      return `
+   <a class="gallery__link" href="${original}">
+     <img
+     class="gallery__image"
+     src="${preview}"
+     alt="${description}"
+     />
+     </a>
+     `;
+    })
+    .join('');
+}
+
+// container.addEventListener('click', openImage);
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  animationSpeed: 150,
+});
+
+gallery.on('show.simplelightbox', function () {
+  evt.preventDefault();
+  console.log(evt);
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+  evt.target.open;
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,29 @@
+import { throttle } from 'lodash';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.elements.email;
+const messageInput = form.elements.message;
+
+form.addEventListener('submit', saveForm);
+form.addEventListener('input', throttle(updateStorage, 1000));
+
+function updateStorage(evt) {
+  evt.preventDefault();
+  const formInput = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formInput));
+  console.log(localStorage.getItem('feedback-form-state'));
+}
+
+const parsedInput = JSON.parse(localStorage.getItem('feedback-form-state'));
+emailInput.value = parsedInput.email || '';
+messageInput.value = parsedInput.message || '';
+
+function saveForm(evt) {
+  evt.preventDefault();
+  form.reset();
+  console.log(parsedInput);
+  localStorage.clear();
+}
